8f4d5320f0b47e3e2a741162f695ac6427b778ed
ANKIT-RAJ-PATEL <ankitraj61370@gmail.com>
2026-08-28 15:21:20 +0530

    fix: remove 401 redirect interceptor causing 404 on Render

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,16 +14,7 @@ axios.interceptors.request.use((config) => {
   return config;
 })
 
-axios.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem("token");
-      window.location.href = "/signin";
-    }
-    return Promise.reject(error);
-  }
-)
+
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
